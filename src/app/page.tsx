9c6f48102c42b089283e1c8d9eb21f11e0fb0cb1
Gerardo SC <gerardo.sc.dev@gmail.com>
2024-01-08 17:36:22 -0600

    add text field component custom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,22 @@ import CInputText from '@/Utils/UIComponents/CInputText/CInputText'
 export default function Home() {
     
   return (
+    <>
     <CInputText 
-      title='USUARIO ado' 
-      placeHolder='usuario' 
-      textHelp='Ingresa usaurio' />
+      title='Usuario' 
+      placeholder='12345678' 
+      textHelp='Ingresa usaurio' 
+      type='text'
+      textValue='12345fsadf'
+      leftIcon='user'/>
+
+    <CInputText 
+      title='Password' 
+      placeholder='12345678' 
+      textHelp='Ingresa contraseña' 
+      type='password'
+      textValue='12345fsadf'
+      leftIcon='key' />
+    </>
   )
 }
