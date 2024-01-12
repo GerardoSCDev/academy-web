b7d58baa3bb6bad4e7db75fcc891aed5e3cb6dfb
Gerardo SC <gerardo.sc.dev@gmail.com>
2024-01-11 21:04:00 -0600

    move login main to login page

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '../globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html className='h-full' lang="en">
+      <body className='h-full' >{children}</body>
     </html>
   )
 }
